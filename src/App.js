532af2a82442508263339e049a47ce3b55c63fb7
Yashemabeth <yashemabethmcleod@gmail.com>
2022-02-23 03:31:05 +0000

    update dthe App.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import Box from "@mui/material/Box";
+import Box from "@mui/material/Container";
+
+import Container from "@mui";
+
 import { ContactForm } from "./components/ContactForm";
 
 export const App = () => {
   return (
     <div>
-      <header>Nav Bar</header>
-      <section>About Me</section>
-      <section>My Projects</section>
-      <section>
-        <ContactForm />
-      </section>
-      <section>Contact Info</section>
+      <box components="header">Nav Bar</box>
+      <container sx={{ border: "1px solid black" }} maxWidth="lg">
+        <box components="section">About Me</box>
+        <box components="section">My Projects</box>
+        <box components="section">
+          <ContactForm />
+        </box>
+      </container>
+      <box>Contact Info</box>
     </div>
   );
 };
